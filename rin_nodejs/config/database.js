072b7df44ad5952dd1d166f709b0b2947582d780
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-12-04 21:29:44 -0500

    Add configurations

---
+++ rin_nodejs/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://rinuser:rindatabase@ds061974.mongolab.com:61974/rin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
