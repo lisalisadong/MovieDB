bc977d3c3af0ec6835b38054398bf235a9fc5ec0
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-12-09 09:40:33 -0500

    change auth

--- rin_nodejs/config/auth.js
+++ rin_nodejs/config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1531115463872992',
         'clientSecret'  : '8aa29b2a3e34e28f07c8319adac5d0f5',
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'http://ec2-54-164-223-242.compute-1.amazonaws.com:3000/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'        : 'your-consumer-key-here',
         'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'        : 'http://ec2-54-164-223-242.compute-1.amazonaws.com:3000/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : 'your-secret-clientID-here',
         'clientSecret'     : 'your-client-secret-here',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : 'http://ec2-54-164-223-242.compute-1.amazonaws.com:3000/auth/google/callback'
     }
 
 };
