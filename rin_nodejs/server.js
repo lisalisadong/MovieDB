e88a6479ea4b98c68cf8165198d45b0b47104809
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-12-14 00:39:16 -0500

    changed log

--- rin_nodejs/server.js
+++ rin_nodejs/server.js
@@ -43,4 +43,4 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('Rin MovieDB on ' + port);
